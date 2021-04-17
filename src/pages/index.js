f5d1d4fa5d49c7afe6c5ae1d8e38cc1147029343
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2021-04-16 20:13:34 -0700

    Header path is fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 import WordCloud from "../components/WordCloud"
-import Header from "../components/header"
+import Header from "../components/Header"
 
 const Container = styled.div`
   margin: 1rem auto;
